adec9678720ef02d8611d691f3c668cb0eb2b469
Pedro Okawa <pedro.henrique.okawa@gmail.com>
2017-05-18 10:51:58 +0100

    * Changed file name of the main image of the portfolio.

--- utils/helper/ImageDBHelper.js
+++ utils/helper/ImageDBHelper.js
@@ -26,45 +26,52 @@ module.exports = {
 
 					return callback(status, response);
 				}
-				
-				var portfolio = response;
-				mkdirp(fileFolderDest,
+
+				fs.removeSync(fileFolderDest,
 					function(err) {
 						if(err) {
-							fs.removeSync(dependenciesManager.output + file.filename,
-								function(err) {
-									if(err) {
-										return callback(500, err);
-									}
-								}
-							);
-
-							return res.json({ error: 'Error while trying to create folder: ' + fileFolderDest });
+							return callback(500, err);
 						}
-
-						var startIndex = file.filename.lastIndexOf(".");
-						var endIndex = file.filename.length;
-						var extension = file.filename.substring(startIndex, endIndex);
-
-						fs.rename(fileFolderSource + '/' + file.filename, fileFolderDest + '/' + portfolioId + extension,
+						
+						var portfolio = response;
+						mkdirp(fileFolderDest,
 							function(err) {
 								if(err) {
-									return callback(500, err);
+									fs.removeSync(dependenciesManager.output + '/' + file.filename,
+										function(err) {
+											if(err) {
+												return callback(500, err);
+											}
+										}
+									);
+
+									return res.json({ error: 'Error while trying to create folder: ' + fileFolderDest });
 								}
 
-								var image = credentials.host + dependenciesManager.imagesFolder + '/' + portfolioId + dependenciesManager.mainImageFolder + '/' + portfolioId + extension;
-								portfolio.image = image;
+								var startIndex = file.filename.lastIndexOf(".");
+								var endIndex = file.filename.length;
+								var extension = file.filename.substring(startIndex, endIndex);
 
-								portfolioDBHelper.update(portfolioId, portfolio,
+								fs.rename(fileFolderSource + '/' + file.filename, fileFolderDest + '/' + file.filename,
 									function(err) {
-										return callback(200, portfolio);
+										if(err) {
+											return callback(500, err);
+										}
+
+										var image = credentials.host + dependenciesManager.imagesFolder + '/' + portfolioId + dependenciesManager.mainImageFolder + '/' + file.filename;
+										portfolio.image = image;
+
+										portfolioDBHelper.update(portfolioId, portfolio,
+											function(err) {
+												return callback(200, portfolio);
+											}
+										);
 									}
 								);
 							}
 						);
 					}
 				);
-
 			}
 		)
 	}
